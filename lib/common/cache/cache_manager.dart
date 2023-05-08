7420b40f92c913107507392889aa93c8f0f10463
kmlgkcy <gmkemalim@gmail.com>
2023-05-08 17:35:18 +0300

    single instance cache manager

--- lib/common/cache/cache_manager.dart
+++ lib/common/cache/cache_manager.dart
@@ -1,30 +1,35 @@
 import 'package:hive/hive.dart';
 
 class CacheManager {
-  String path;
-  CacheManager({
-    required this.path,
-  });
+  static CacheManager? _instance;
+  static CacheManager get instance {
+    _instance ??= CacheManager._init();
+    return _instance!;
+  }
 
-  Future<dynamic> read({String? key}) async {
+  CacheManager._init();
+  Future<dynamic> read(String path, {String? key}) async {
     var box = await Hive.openBox(path);
+    // await drop();
     if (key == null) {
+      // box.toString();
+      // box.values.log();
       return box.values;
     }
     return box.get(key);
   }
 
-  Future<dynamic> write({required String id, required String data}) async {
+  Future<dynamic> write(String path, {required String id, required String data}) async {
     var box = await Hive.openBox(path);
     return box.put(id, data);
   }
 
-  Future<dynamic> delete({required String id}) async {
+  Future<dynamic> delete(String path, {required String id}) async {
     var box = await Hive.openBox(path);
     return box.delete(id);
   }
 
-  Future<dynamic> drop() async {
+  Future<dynamic> drop(String path) async {
     var box = await Hive.openBox(path);
     return box.deleteFromDisk();
   }
